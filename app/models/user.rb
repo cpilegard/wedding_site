97b955687eedb865d3525a0ceb41684fdf34b161
Cole Pilegard <c.pilegard@gmail.com>
2013-08-01 20:44:18 -0700

    add carly to mailgun

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     RestClient.post "https://api:key-04159sfvisu54yab495nsgrf0-an8n84"\
     "@api.mailgun.net/v2/coleandcarly.mailgun.org/messages",
     :from => "Cole and Carly's wedding site <me@coleandcarly.mailgun.org>",
-    :to => "c.pilegard@gmail.com",
+    :to => "c.pilegard@gmail.com, carlycogburn@gmail.com",
     :subject => "Someone RSVP'd!",
     :text => "NAME: #{self.name}, STATUS: #{self.status}"
   end
